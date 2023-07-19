2e6e5ecbc83eba0638bf9609ba621d5737bebad1
JJangSSo <jsooooY@gmail.com>
2023-07-19 16:31:27 +0900

    커뮤니티 계속 영차 영차

--- src/main/java/com/ssofun/www/community/mapper/CommunitySqlMapper.java
+++ src/main/java/com/ssofun/www/community/mapper/CommunitySqlMapper.java
@@ -1,5 +1,9 @@
 package com.ssofun.www.community.mapper;
 
+import com.ssofun.dto.CommunityDto;
+
 public interface CommunitySqlMapper {
 
+	
+	public void insert(CommunityDto communityDto);
 }
--- src/main/java/com/ssofun/www/community/service/CommunityService.java
+++ src/main/java/com/ssofun/www/community/service/CommunityService.java
@@ -1,8 +1,19 @@
 package com.ssofun.www.community.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ssofun.www.community.mapper.CommunitySqlMapper;
 
 @Service
 public class CommunityService {
+	
+	@Autowired
+	private CommunitySqlMapper communitySqlMapper;
+	@Autowired
+	private 
+	
+	
+	
 
 }
