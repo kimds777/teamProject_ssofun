3153506bc1ba95cd67379ad70bcbfcd6e15b32ce
kimds777 <tina27849@gmail.com>
2023-07-27 12:10:21 +0900

    배달dto 수정 및 업로드

--- src/main/java/com/ssofun/dto/DeliveryDto.java
+++ src/main/java/com/ssofun/dto/DeliveryDto.java
@@ -2,14 +2,18 @@ package com.ssofun.dto;
 
 import java.util.Date;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class DeliveryDto {
 	private int delivery_id; // PK
 	private int delivery_company_id; // FK
+	private long delivery_status_id;
 	private int delivery_recipient_id; // FK
 	private String invoice_no; // 운송장번호
-	@JsonFormat(pattern = "yyyy-MM-dd:HH.mm.ss",  timezone = "Asia/Seoul") //데이터 포맷 변환
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy.MM.dd")
+	@DateTimeFormat(pattern = "yyyy.MM.dd")
 	private Date created_at; // 등록일
 	private int used_fg; // 여부
 	
@@ -17,16 +21,33 @@ public class DeliveryDto {
 		super();
 	}
 
-	public DeliveryDto(int delivery_id, int delivery_company_id, int delivery_recipient_id, String invoice_no,
-			Date created_at, int used_fg) {
+
+
+	public DeliveryDto(int delivery_id, int delivery_company_id, long delivery_status_id, int delivery_recipient_id,
+			String invoice_no, Date created_at, int used_fg) {
 		super();
 		this.delivery_id = delivery_id;
 		this.delivery_company_id = delivery_company_id;
+		this.delivery_status_id = delivery_status_id;
 		this.delivery_recipient_id = delivery_recipient_id;
 		this.invoice_no = invoice_no;
 		this.created_at = created_at;
 		this.used_fg = used_fg;
 	}
+
+
+
+	public long getDelivery_status_id() {
+		return delivery_status_id;
+	}
+
+
+
+	public void setDelivery_status_id(long delivery_status_id) {
+		this.delivery_status_id = delivery_status_id;
+	}
+
+
 
 	public int getDelivery_id() {
 		return delivery_id;
