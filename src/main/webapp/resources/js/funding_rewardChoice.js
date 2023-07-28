bd78fbfa1c77c8e79fe1d121dd5ec38c8d5ba8d5
kimds777 <tina27849@gmail.com>
2023-07-28 11:08:34 +0900

    펀딩 리워드 선택페이지 수정

--- src/main/webapp/resources/js/funding_rewardChoice.js
+++ src/main/webapp/resources/js/funding_rewardChoice.js
@@ -288,7 +288,6 @@ function getFundingDto($funding_id,$funding_reward_id){
                         var buy_count;
                         $.each(item,function(key,value){
                             if(key == "funding_reward_id"){
-                                alert("리워드 리스트 리워드별 아이디: "+value);
                                 funding_reward_id = value;
                             }
                             if(key == "price"){
